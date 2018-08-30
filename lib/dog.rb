0eb5368951f18dbdda9875fe3b55c356c830ac5d
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-30 13:06:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,8 @@ class Dog
   def initialize(name, breed)
     
     @name = name
-    
-    if breed.empty?
-      @breed = "Mutt"
-    else
-      @breed = breed
-    end
-    
+    @breed = "Mutt"
+  
 end
 
 attr_accessor :breed
